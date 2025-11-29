64f9e96ca99e6ac6973a49dfa079740781854081
capaooow <paolawitrago@outlook.com>
2025-11-29 00:34:35 -0600

    docs: agregar minuta del daily scrum 1 - sprint 1

--- Crear_Muuusica.sql
+++ Crear_Muuusica.sql
@@ -1,156 +1,208 @@
-CREATE DATABASE BD_Muuusica;
-USE BD_Muuusica; 
+CREATE DATABASE muuusica;
+USE muuusica;
 
-DELIMITER $$
-CREATE PROCEDURE SP_CREA_BD_Muuusica ()
-BEGIN
-
-CREATE TABLE Usuario(
-	UsuarioID int auto_increment,
-    primary key(UsuarioID),
-    NombreUsuario varchar(50), 
-    ApellidoUsuario varchar(50),
-    ContrasenaUsuario varchar(50),
-    EmaiUsuario varchar(30),
-    Foto_Perfil_URL varchar(100),
-    DescripcionUsuario varchar(500),
-    CancionPerfilUsuario varchar(100),
-    Email_Verificado boolean default false,
-    Es_Admin boolean default false
+CREATE TABLE albumes (
+	album_id INT AUTO_INCREMENT,
+    PRIMARY KEY(album_id),
+    titulo VARCHAR(50),
+	imagen_portada_path VARCHAR(500),
+    fecha_lanzamiento DATE,
+    permitir_descarga BOOLEAN
 );
 
-CREATE TABLE Genero (
-	GeneroID int auto_increment,
-    primary key(GeneroID),
-    Nombre_Genero varchar(25),
-    Imagen_Genero varchar(100)
+CREATE TABLE canciones (
+	cancion_id INT AUTO_INCREMENT,
+    PRIMARY KEY(cancion_id),
+    album_id INT,
+    titulo VARCHAR(15), 
+    duracion TIME,
+    ruta_archivo VARCHAR(200),
+    tamano_archivo BIGINT,
+    num_reproducciones INT,
+    permitir_descarga BOOLEAN,
+    FOREIGN KEY(album_id) REFERENCES albumes(album_id)
+);
+
+CREATE TABLE usuarios (
+	usuario_id INT AUTO_INCREMENT,
+    PRIMARY KEY(usuario_id),
+    cancion_id INT, -- Canción de perfil
+    nombre VARCHAR(50), 
+    apellido VARCHAR(50),
+    contrasena VARCHAR(50),
+    email VARCHAR(30),
+    foto_perfil_path VARCHAR(100),
+    descripcion VARCHAR(500),
+	es_email_verificado BOOLEAN DEFAULT FALSE,
+    es_admin BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id)
+);
+
+CREATE TABLE generos (
+	genero_id INT AUTO_INCREMENT,
+    PRIMARY KEY(genero_id),
+    nombre VARCHAR(25),
+    imagen_path VARCHAR(100)
 );  
 
-CREATE TABLE Genero_Usuario (
-    GeneroUsuarioID int auto_increment,
-    primary key(GeneroUsuarioID),
-    R_Usuario int,
-    R_Genero int,
-    FOREIGN KEY (R_Usuario) REFERENCES Usuario(UsuarioID),
-    FOREIGN KEY (R_Genero) REFERENCES Genero(GeneroID),
-    UNIQUE KEY unique_usuario_genero (R_Usuario, R_Genero)
+CREATE TABLE usuarios_generos (
+    usuario_genero_id INT AUTO_INCREMENT,
+    PRIMARY KEY(usuario_genero_id),
+    usuario_id INT,
+    genero_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (genero_id) REFERENCES generos(genero_id),
+    UNIQUE KEY unique_usuario_genero (usuario_id, genero_id)
 );
 
-CREATE TABLE Artista (
-	ArtistaID int auto_increment,
-    primary key(ArtistaID),
-    NombreArtista varchar(30),
-    ShipperPhone varchar(15)
+CREATE TABLE artistas (
+	artista_id INT AUTO_INCREMENT,
+    PRIMARY KEY(artista_id),
+    nombre VARCHAR(30),
+    biografia VARCHAR(500),
+    sitio_web_url VARCHAR(100),
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)
 );
 
-CREATE TABLE Album (
-	AlbumID int auto_increment,
-    primary key(AlbumID),
-    R_Artista int,
-    R_Genero int,
-    Titulo_Album varchar(50),
-    Portada_Album varchar(500),
-	FOREIGN KEY (R_Artista) REFERENCES Artista(ArtistaID),
-    FOREIGN KEY (R_Genero) REFERENCES Genero(GeneroID)
+CREATE TABLE albumes_artistas (
+	rol ENUM('Principal', 'Productor'),
+    album_id INT,
+    artista_id INT,
+    FOREIGN KEY (album_id) REFERENCES albumes(album_id),
+    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 );
 
-CREATE TABLE Musica (
-	MusicaID int auto_increment,
-    primary key(MusicaID),
-    R_Artista int, 
-    R_Album int,
-    Titulo_Cancion varchar(15), 
-    Duracion int,
-    Ruta_Archivo_M varchar(200),
-    Tamano_Archivo_M int,
-    Num_Reproducciones int,
-    FOREIGN KEY(R_Artista) REFERENCES Artista(ArtistaID),  
-    FOREIGN KEY(R_Album) REFERENCES Album(AlbumID)
+CREATE TABLE albumes_generos (
+	album_id INT,
+    genero_id INT,
+    FOREIGN KEY (album_id) REFERENCES albumes(album_id),
+    FOREIGN KEY (genero_id) REFERENCES generos(genero_id)
 );
 
-CREATE TABLE Favorito (
-	FavoritoID int auto_increment, 
-    primary key(FavoritoID),
-    R_Usuario int,  
-    R_Musica int,  
-    Es_Corazon boolean default false,
-    FOREIGN KEY(R_Usuario) REFERENCES Usuario(UsuarioID),  
-    FOREIGN KEY(R_Musica) REFERENCES Musica(MusicaID)     
+CREATE TABLE canciones_artistas (
+	tipo_participacion ENUM('Principal', 'Featuring'),
+    cancion_id INT,
+    artista_id INT,
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id),
+    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 );
 
-CREATE TABLE Playlist (
-	PlaylistID int auto_increment,
-    primary key(PlaylistID),
-    R_Usuario int,
-    Nombre_Playlist varchar(30),
-    Es_Publica boolean default false,
-    FOREIGN KEY (R_Usuario) REFERENCES Usuario(UsuarioID)  
+CREATE TABLE canciones_generos (
+	cancion_id INT,
+    genero_id INT,
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id),
+    FOREIGN KEY (genero_id) REFERENCES generos(genero_id)
 );
 
-CREATE TABLE Playlist_Cancion (
-	R_Playlist int,
-	R_Musica int,
-    Orden int,
-	PRIMARY KEY (R_Playlist, R_Musica),    
-    FOREIGN KEY (R_Playlist) REFERENCES Playlist(PlaylistID),  
-    FOREIGN KEY (R_Musica) REFERENCES Musica(MusicaID)
+CREATE TABLE favoritos_albumes (
+    usuario_id INT,  
+    album_id INT,  
+    es_favorito BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),  
+    FOREIGN KEY (album_id) REFERENCES albumes(album_id)     
 );
 
-CREATE TABLE Resena(
-    ResenaID int auto_increment,
-    primary key(ResenaID),
-	R_Usuario int,
-	R_Musica int,
-    R_Album int,
-    Titulo_Resena varchar(30),
-    Contenido_Resena varchar(200),
-    Calificacion_Estrellas int check (Calificacion_Estrellas between 1 and 3), 
-    FOREIGN KEY (R_Usuario) REFERENCES Usuario(UsuarioID),
-    FOREIGN KEY (R_Musica) REFERENCES Musica(MusicaID),
-    FOREIGN KEY (R_Album) REFERENCES Album(AlbumID)
-);  
-
-CREATE TABLE Reaccion_Resena (
-	ReaccionID int auto_increment,
-    Primary key(ReaccionID),  
-    R_Usuario int,
-    R_Resena int,
-    Tipo_Reaccion ENUM('like', 'dislike'),  
-    FOREIGN KEY (R_Usuario) REFERENCES Usuario(UsuarioID),
-    FOREIGN KEY (R_Resena) REFERENCES Resena(ResenaID)
+CREATE TABLE favoritos_canciones (
+    usuario_id INT,  
+    cancion_id INT,  
+    es_favorito BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),  
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id)     
 );
 
-CREATE TABLE Calificacion (
-	CalificacionID int auto_increment,
-    Primary key(CalificacionID),  
-    R_Usuario int,
-    R_Musica int,
-    R_Album int,
-    Puntuacion int check(Puntuacion between 1 and 5), 
-    FOREIGN KEY (R_Usuario) REFERENCES Usuario(UsuarioID),
-    FOREIGN KEY (R_Musica) REFERENCES Musica(MusicaID),
-    FOREIGN KEY (R_Album) REFERENCES Album(AlbumID)
+CREATE TABLE playlists (
+	playlist_id INT AUTO_INCREMENT,
+    PRIMARY KEY (playlist_id),
+    usuario_id INT,
+    nombre VARCHAR(30),
+    fecha_creacion DATE,
+    imagen_portada VARCHAR(500),
+    es_publica BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)  
+);
+
+CREATE TABLE playlists_canciones (
+	playlist_id INT,
+	cancion_id INT,
+    orden INT,   
+    FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id),  
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id)
+);
+
+CREATE TABLE resenas_albumes (
+	resena_album_id INT AUTO_INCREMENT,
+    PRIMARY KEY (resena_album_id),
+	usuario_id INT,
+    album_id INT,
+    comentario VARCHAR(200),
+    fecha_resena DATE,
+    puntuacion_estrellas INT CHECK (puntuacion_estrellas BETWEEN 1 AND 5), 
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (album_id) REFERENCES albumes(album_id)
+);
+
+CREATE TABLE resenas_canciones (
+	resena_cancion_id INT AUTO_INCREMENT,
+    PRIMARY KEY (resena_cancion_id),
+	usuario_id INT,
+    cancion_id INT,
+    comentario VARCHAR(200),
+    fecha_resena DATE,
+    puntuacion_estrellas INT CHECK (puntuacion_estrellas BETWEEN 1 AND 5), 
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id)
+);
+
+CREATE TABLE reacciones_resenas_canciones (
+    usuario_id INT,
+    resena_cancion_id INT,
+    tipo ENUM('like', 'dislike'),  
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (resena_cancion_id) REFERENCES resenas_canciones(resena_cancion_id)
+);
+
+CREATE TABLE reacciones_resenas_albumes (
+    usuario_id INT,
+    resena_album_id INT,
+    tipo ENUM('like', 'dislike'),  
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (resena_album_id) REFERENCES resenas_albumes(resena_album_id)
+);
+
+CREATE TABLE descargas_canciones (
+	-- descarga_cancion_id INT AUTO_INCREMENT,
+    -- PRIMARY KEY (descarga_cancion_id), 
+    usuario_id INT,
+    cancion_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (cancion_id) REFERENCES canciones(cancion_id)
+);
+
+CREATE TABLE descargas_albumes (
+	-- descarga_album_id INT AUTO_INCREMENT,
+    -- PRIMARY KEY (descarga_album_id), 
+    usuario_id INT,
+    album_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (album_id) REFERENCES albumes(album_id)
+);
+
+CREATE TABLE descargas_playlists (
+	-- descarga_playlist_id INT AUTO_INCREMENT,
+    -- PRIMARY KEY (descarga_playlist_id), 
+    usuario_id INT,
+    playlist_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (playlist_id) REFERENCES playlists(playlist_id)
 ); 
 
-CREATE TABLE Descarga (
-	DescargaID int auto_increment,
-    Primary key(DescargaID), 
-    R_Usuario int,
-    R_Musica int,
-    FOREIGN KEY (R_Usuario) REFERENCES Usuario(UsuarioID),
-    FOREIGN KEY (R_Musica) REFERENCES Musica(MusicaID)
-);  
-
-CREATE TABLE API_Externa ( 
-	APIID int auto_increment,
-    primary key(APIID),
-	Nombre_API varchar(100),    
-	API_Key varchar(500),         
-	Endpoint_URL varchar(500),             
-	Activa boolean default false
+CREATE TABLE api_externa ( 
+	api_id INT AUTO_INCREMENT,
+    PRIMARY KEY (api_id),
+	nombre VARCHAR(100),    
+	api_key VARCHAR(500),         
+	endpoint_url VARCHAR(500),             
+	activa BOOLEAN DEFAULT FALSE
 ); 
 
-END $$
-DELIMITER ;
-
-CALL SP_CREA_BD_Muuusica();
